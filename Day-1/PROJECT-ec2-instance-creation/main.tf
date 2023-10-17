204a1379f0c57291b817d457a832ea442c810538
ayanav <aryan.mitra3@gmail.com>
2023-10-17 16:58:34 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,3 +1,4 @@
+
 provider "aws" {
     region = "ap-south-1"  # Set your desired AWS region
 }
@@ -6,3 +7,5 @@ resource "aws_instance" "example" {
     ami           = "ami-0a5ac53f63249fba0"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
+# Secret Key: sr9LPgpIxJgEGPVZ1ETe5+3BRdjscRQZchg9Jblp
+# Access Key: AKIAR42UHEJF6Q5YDPC6
