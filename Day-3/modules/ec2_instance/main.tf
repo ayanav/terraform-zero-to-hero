d3dd5e218f78aea901e11e0adfea16877b667a2a
ayanav <aryan.mitra3@gmail.com>
2023-10-18 21:29:15 +0530

    Update main.tf

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -1,9 +1,19 @@
 provider "aws" {
-  region = "us-east-1"
+    region = "ap-south-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
     ami = var.ami_value
     instance_type = var.instance_type_value
-    subnet_id = var.subnet_id_value
+    key_name = var.key_name
+    security_groups = [ var.security_groups ]
 }
+
+
+
+#resource "aws_instance" "example" {
+#    ami           = "ami-0a5ac53f63249fba0"  # Specify an appropriate AMI ID
+#    instance_type = "t2.micro"
+#    key_name = "firstec2"
+#    security_groups = [ "launch-wizard-1" ]
+#}
