059b957c9aeb4a30a2f4d3df07c49f7af8447585
ayanav <aryan.mitra3@gmail.com>
2023-10-18 21:46:23 +0530

    Update outputs.tf

--- Day-3/modules/ec2_instance/outputs.tf
+++ Day-3/modules/ec2_instance/outputs.tf
@@ -1,3 +1,11 @@
 output "public-ip-address" {
   value = aws_instance.example.public_ip
 }
+
+output "public-dns" {
+  value = aws_instance.example.public_dns
+}
+
+output "host-id" {
+  value = aws_instance.example.id
+}
