242d042270b96e9ac35fd6121c2bf0d1b4193f91
ayanav <aryan.mitra3@gmail.com>
2023-10-25 17:24:24 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
-    region         = "us-east-1"
+    bucket         = "ayanava-s3-demo-xyz" # change this
+    key            = "ayanava/terraform.tfstate"
+    region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform_lock"
   }
 }
