ebf744785be7a8642fcdf00b94958fda2f1aad37
ayanav <aryan.mitra3@gmail.com>
2023-10-25 17:23:01 +0530

    Update main.tf

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -1,19 +1,20 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "ayanava" {
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  ami = "ami-0a5ac53f63249fba0" # change this
+  key_name = "firstec2"
+  security_groups = ["launch-wizard-1"]
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+/*resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "ayanava-s3-demo-xyz" # change this
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock"
+  name           = "terraform_lock"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
 
@@ -21,4 +22,4 @@ resource "aws_dynamodb_table" "terraform_lock" {
     name = "LockID"
     type = "S"
   }
-}
+}*/
